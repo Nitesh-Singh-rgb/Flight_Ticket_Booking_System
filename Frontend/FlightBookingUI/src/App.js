04253f318b5d3b002376e2b1523bc0a395651d14
Nitesh-Singh-rgb <nsingh2931@gmail.com>
2024-08-21 19:43:41 +0530

    homepage_update

--- Frontend/FlightBookingUI/src/App.js
+++ Frontend/FlightBookingUI/src/App.js
@@ -1,5 +1,12 @@
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
+import Info from "./components/Info/Info";
+import Lounge from "./components/Lounge/Lounge";
 import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+import Subscribers from "./components/Subscribers/Subscribers";
+import Support from "./components/Support/Support";
+import Travelers from "./components/Travelers/Travelers";
 
 
 function App() {
@@ -7,6 +14,13 @@ function App() {
     <main>
       <Navbar />
       <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      {/* <Travelers /> */}
+      <Subscribers />
+      <Footer />
     </main>
   );
 }
---
+++ Frontend/FlightBookingUI/src/components/Subscribers/Subscribers.js
@@ -0,0 +1,15 @@
+const Subscribers = () => {
+    return (
+        <div className="subscribe section">
+            <div className="sectionContainer container">
+                <h2>Subscribe Newsletter to get Latest News</h2>
+                <div className="inputDiv flex">
+                    <input type="email" placeholder="Enter your email address" />
+                    <button className="btn">Subscribe</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Subscribers
